457ee82a907d8fc94c57c48d96cd1fed9f3e5c5c
bschkuhl <bschkuhl@googlemail.com>
2024-12-05 20:49:50 +0100

    added genre proximity filter

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import logging
 from datetime import datetime
 import unicodedata
 import Levenshtein
+import re
 
 # Load environment variables from the .env file
 load_dotenv()
@@ -18,8 +19,10 @@ SPOTIFY_REDIRECT_URI = os.getenv("REDIRECT_URI")
 SCOPE = 'playlist-modify-public playlist-modify-private'
 BATCH_SIZE = 100
 LISTS_DIR = "./lists"
-LIST_JSON = "festivals.json" # Replace with your own JSON file
+LIST_JSON = "test.json" # Replace with your own JSON file
 LOG_DIR = './logs'
+COUNTRY_PREFIXES_JSON = "country_prefixes.json" # Replace with your own JSON file
+HELPER_DIR = './helpers'
 # Configuration variable to decide whether to pick the higher popularity or skip on finding duplicate artists
 PICK_HIGHER_POPULARITY = True 
 # Configuration variable to decide whether to clear playlist before adding new songs
@@ -27,6 +30,8 @@ CLEAR_PLAYLIST = True
 # Configuration variable to allow approximate matches with distance of x
 USE_LEV = False 
 APPROX_MATCHES = 1 
+# Configuration vairable to decide whether to pick the artist that is more similar in genre or skip on finding duplicate artists
+PICK_GENRE_PROXIMITY = True # Only works with larger playlists, genre sometimes not available for smaller artists
 
 os.makedirs(LOG_DIR, exist_ok=True)
 
@@ -78,16 +83,35 @@ def normalize_string(s):
     """Normalize string to remove special characters and accents."""
     return unicodedata.normalize('NFKD', s).encode('ASCII', 'ignore').decode('ASCII').lower()
 
+def normalize_genre(genres):
+    normalized_genres = []
+    # Create a regex pattern to match any of the country prefixes followed by a space
+    pattern = re.compile(rf"^({'|'.join(re.escape(prefix) for prefix in country_prefix_file['country_prefixes'])})\s+", re.IGNORECASE)
+    # Substitute the matched prefix with an empty string
+    for genre in genres:
+        normalized_genre = pattern.sub('', genre)
+        normalized_genres.append(normalized_genre)
+        if normalized_genre != genre:
+            logging.info(f'Normalized genre "{genre}" to "{normalized_genre}".')
+    return normalized_genres
+
 # Load playlist data from file
 file_path = os.path.join(LISTS_DIR, LIST_JSON)
 with open(file_path, "r") as file:
     playlist_file = json.load(file)
 
+# Load playlist data from file
+c_prefix_path = os.path.join(HELPER_DIR, COUNTRY_PREFIXES_JSON)
+with open(c_prefix_path, "r") as file:
+    country_prefix_file = json.load(file)
+
 # Access and process playlists
 user_id = sp.me()['id']
 for playlist in playlist_file["playlists"]:
     track_uris = []
-
+    exact_matches_groups = []
+    genre_list = []
+    
     # Check if the playlist already exists
     existing_playlist_id = playlist_exists(user_id, playlist["playlistName"])
     if existing_playlist_id:
@@ -111,29 +135,31 @@ for playlist in playlist_file["playlists"]:
     # Process artists and their top tracks
     for artist in playlist["artists"]:
     # Perform search query with the artist name
+        normalized_artistname = normalize_string(artist)
         results = sp.search(q=f'{artist}', type='artist', limit=50)  # Increase limit to handle multiple exact matches
 
         # Filter results for exact name matches (case-insensitive)
-        exact_matches = [item for item in results['artists']['items'] if normalize_string(item['name']) == normalize_string(artist)]
+        exact_matches = [item for item in results['artists']['items'] if normalize_string(item['name']) == normalized_artistname]
 
         if not exact_matches and USE_LEV:
-            if not exact_matches:
-                approximate_matches = [
-                    item for item in results['artists']['items']
-                    if Levenshtein.distance(normalize_string(item['name']), normalize_string(artist)) == APPROX_MATCHES
-                ]
+            approximate_matches = [
+                item for item in results['artists']['items']
+                if Levenshtein.distance(normalize_string(item['name']), normalized_artistname) == APPROX_MATCHES
+            ]
 
-                if approximate_matches:
-                    logging.info(f"Found approximate matches for artist '{artist}': {[item['name'] for item in approximate_matches]}")
-                    exact_matches = approximate_matches  # Treat approximate matches as exact matches for further processing
-                else:
-                    logging.info(f"No matches (exact or approximate) found for artist '{artist}'")
+            if approximate_matches:
+                logging.info(f"Found approximate matches for artist '{artist}': {[item['name'] for item in approximate_matches]}")
+                exact_matches = approximate_matches  # Treat approximate matches as exact matches for further processing
+            else:
+                logging.info(f"No matches (exact or approximate) found for artist '{artist}'")
         elif exact_matches:
             if len(exact_matches) > 1:
                 # Log multiple exact matches
                 logging.info(f"Multiple exact matches found for artist '{artist}': {[match['name'] for match in exact_matches]}")
-                
-                if PICK_HIGHER_POPULARITY:
+                if PICK_GENRE_PROXIMITY:
+                    exact_matches_groups.append(exact_matches)
+                    continue
+                elif not PICK_GENRE_PROXIMITY and PICK_HIGHER_POPULARITY:
                     # Choose the match with the highest popularity
                     best_match = max(exact_matches, key=lambda x: x['popularity'])
                     logging.info(f"Choosing the most popular match for artist '{artist}': {best_match['name']} with popularity {best_match['popularity']}")
@@ -147,13 +173,49 @@ for playlist in playlist_file["playlists"]:
         
             # Process the best match
             artist_id = best_match['id']
+            genre_list = genre_list + normalize_genre(best_match['genres'])
             top_tracks = sp.artist_top_tracks(artist_id)['tracks'][:3]
             
             # Extend track URIs if not already present
             track_uris.extend([track['uri'] for track in top_tracks if track['uri'] not in existing_tracks])
         else:
-                logging.info(f"No exact match found for artist '{artist} in:'")
-                logging.info(f"{[item['name'].lower() for item in results['artists']['items']]}")
+            logging.info(f"No exact match found for artist '{artist} in:'")
+            logging.info(f"{[item['name'].lower() for item in results['artists']['items']]}")
+
+    if PICK_GENRE_PROXIMITY:
+        for group in exact_matches_groups:
+            # check if per artist in match groups the genres and count how many matches. add to genre_matches list 
+            # Find the item(s) with the highest number of matches
+            max_matches = 0
+            genre_matches = []
+            for artist in group:
+                if artist['name']  == 'Khan':
+                    cool = 1
+                matches = len(set(genre_list) & set( artist['genres']))
+                if matches > max_matches:
+                    max_matches = matches
+                    genre_matches = [artist]  # Reset to this item
+                elif matches == max_matches and matches > 0:
+                    genre_matches.append(artist)  # Add to the list of best items
+            
+            if len(genre_matches) > 1: 
+                best_match = max(genre_matches, key=lambda x: x['popularity'])
+                logging.info(f"Choosing the most popular match for artist '{artist}': {best_match['name']} with popularity {best_match['popularity']}")
+            elif len(genre_matches) == 1: 
+                best_match = genre_matches[0]  # Take the first item if there's only one match
+                logging.info(f"Choosing the best genre match for artist '{artist}': {best_match['name']} with genre match rating {max_matches}")
+            elif PICK_HIGHER_POPULARITY:
+                best_match = max(group, key=lambda x: x['popularity'])
+                logging.info(f"Choosing the most popular match for artist '{artist}': {best_match['name']} with popularity {best_match['popularity']}")
+            else: 
+                continue
+
+            # Process the best match
+            artist_id = best_match['id']
+            top_tracks = sp.artist_top_tracks(artist_id)['tracks'][:3]
+            
+            # Extend track URIs if not already present
+            track_uris.extend([track['uri'] for track in top_tracks if track['uri'] not in existing_tracks])
 
     # Add new tracks to the playlist
     if track_uris:
